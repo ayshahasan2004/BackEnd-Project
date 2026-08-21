9ed3745efcac136c7cf573e21471bc75f1b7e78a
Aysha Hasan <1220352@student.birzeit.edu>
2026-08-21 10:40:45 +0300

    add MenuService mapping category/item entities to DTO

---
+++ src/main/java/com/cafe/cafeBackend/service/MenuService.java
@@ -0,0 +1,47 @@
+package com.cafe.cafeBackend.service;
+
+import com.cafe.cafeBackend.dto.MenuCategoryDto;
+import com.cafe.cafeBackend.dto.MenuItemDto;
+import com.cafe.cafeBackend.model.MenuCategory;
+import com.cafe.cafeBackend.model.MenuItem;
+import com.cafe.cafeBackend.repository.MenuCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor /// create arg constructor take the private menuCategoryRepository
+public class MenuService {
+
+    private final MenuCategoryRepository menuCategoryRepository;
+
+    public List<MenuCategoryDto> getMenu() {
+        List<MenuCategory> categories = menuCategoryRepository.findAll();
+        List<MenuCategoryDto> result = new ArrayList<>();
+
+        for (MenuCategory category : categories) {
+
+            List<MenuItemDto> itemDtos = new ArrayList<>();
+            for (MenuItem item : category.getItems()) {
+                MenuItemDto itemDto = new MenuItemDto(
+                        String.valueOf(item.getId()),
+                        item.getName(),
+                        item.getNote(),
+                        item.getPrice()
+                );
+                itemDtos.add(itemDto);
+            }
+
+            MenuCategoryDto categoryDto = new MenuCategoryDto(
+                    category.getName(),
+                    itemDtos
+            );
+
+            result.add(categoryDto);
+        }
+
+        return result;
+    }
+}
